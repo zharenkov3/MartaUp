7548e39d30a2876c7a564fb85132da0929c00be6
Artem Zharenkov <zharenkov3@yandex.ru>
2021-05-12 21:58:32 +0300

    Добавляет адаптацию

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,18 @@
-$(function() {
-	$('.header-slider').slick({
-		arrows: false,
-		vertical: true,
-		dots: true,
-		dotsClass: 'header-dots',
-		autoplay: 2000
-	});
+// $(function() {
 
-});
+// 	$('.header-slider').slick({
+// 		arrows: false,
+// 		vertical: true,
+// 		dots: true,
+// 		dotsClass: 'header-dots',
+// 		// autoplay: 2000
+// 	});
+
+// 	$('.main-nav__btn').on('click', function(){
+// 		$('main-nav__site-list').slideToggle();
+// 	});
+
+// });
+
+
+
